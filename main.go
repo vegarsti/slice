1ff7844a28a243e8dfdce9fdeaa0b9182444eae7
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2021-03-20 18:33:09 +0100

    Extract into functions

--- main.go
+++ main.go
@@ -12,14 +12,14 @@ import (
 )
 
 func main() {
-	from, to, err := parseArgs(os.Args)
+	from, to, err := parseArgs(os.Args[1:])
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%v", err)
 		fmt.Fprintf(os.Stderr, "\n")
 		fmt.Fprintf(os.Stderr, "usage: slice from:to\n")
 		os.Exit(1)
 	}
-	if err := write(os.Stdin, os.Stdout, from, to); err != nil {
+	if err := slice(os.Stdin, os.Stdout, from, to); err != nil {
 		fmt.Fprintf(os.Stderr, "write: %v\n", err)
 		os.Exit(1)
 	}
@@ -29,28 +29,28 @@ func parseArgs(args []string) (int, int, error) {
 	var from, to int
 	var err error
 
-	if len(args) == 1 {
-		return 0, 0, fmt.Errorf("no cutstring specified")
+	if len(args) == 0 {
+		return 0, 0, fmt.Errorf("no range specified")
 	}
-	if len(args) > 2 {
-		return 0, 0, fmt.Errorf("more than one argument")
+	if len(args) > 1 {
+		return 0, 0, fmt.Errorf("only takes one argument")
 	}
-	cutstring := args[1]
-	x := strings.Split(cutstring, ":")
-	if len(x) > 2 && x[0] != "" {
-		return 0, 0, fmt.Errorf("invalid cutstring format: must be `from:` or `:to` or `from:to`")
+	arg := args[0]
+	fromTo := strings.Split(arg, ":")
+	if len(fromTo) > 2 {
+		return 0, 0, fmt.Errorf("invalid range format: must be `from:` or `:to` or `from:to`")
 	}
-	if x[0] == "" {
-		x[0] = "0"
+	if fromTo[0] == "" {
+		fromTo[0] = "0"
 	}
-	from, err = strconv.Atoi(x[0])
+	from, err = strconv.Atoi(fromTo[0])
 	if err != nil {
-		return 0, 0, fmt.Errorf(fmt.Sprintf("invalid cutstring format: %v", err))
+		return 0, 0, fmt.Errorf(fmt.Sprintf("invalid range format: %v", err))
 	}
-	if len(x) == 2 && x[1] != "" {
-		to, err = strconv.Atoi(x[1])
+	if len(fromTo) == 2 && fromTo[1] != "" {
+		to, err = strconv.Atoi(fromTo[1])
 		if err != nil {
-			return 0, 0, fmt.Errorf(fmt.Sprintf("invalid cutstring format: %v", err))
+			return 0, 0, fmt.Errorf(fmt.Sprintf("invalid range format: %v", err))
 		}
 		if to == 0 {
 			return 0, 0, fmt.Errorf("to cannot be 0")
@@ -62,33 +62,13 @@ func parseArgs(args []string) (int, int, error) {
 	return from, to, nil
 }
 
-func write(in io.Reader, out io.Writer, from int, to int) error {
+func slice(in io.Reader, out io.Writer, from int, to int) error {
 	s := bufio.NewScanner(in)
 	w := bufio.NewWriter(out)
-	var fromm, too int
 	for s.Scan() {
 		line := s.Text()
-		too = to
-		fromm = from
-
-		// handle negative slices
-		if fromm < 0 {
-			fromm = len(line) + fromm
-		}
-		if too < 0 {
-			too = len(line) + too
-		}
-
-		// handle variable length
-		if too == 0 || too > len(line) {
-			too = len(line)
-		}
-
-		if from > too {
-			w.WriteString("")
-		} else {
-			w.WriteString(line[fromm:too])
-		}
+		slicedLine := sliceLine(line, from, to)
+		w.WriteString(slicedLine)
 		w.WriteString("\n")
 	}
 	w.Flush()
@@ -96,4 +76,24 @@ func write(in io.Reader, out io.Writer, from int, to int) error {
 		return err
 	}
 	return nil
+}
+
+func sliceLine(line string, from int, to int) string {
+	// handle negative slices
+	if from < 0 {
+		from = len(line) + from
+	}
+	if to < 0 {
+		to = len(line) + to
+	}
+
+	// handle variable length
+	if to == 0 || to > len(line) {
+		to = len(line)
+	}
+
+	if from > to {
+		return ""
+	}
+	return line[from:to]
 }
