6c41bb4b3b6b28dfe817c46ae36d834d8eff11bc
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2021-03-20 18:33:56 +0100

    Add tests

---
+++ main_test.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestParseArgsOK(t *testing.T) {
+	tcs := []struct {
+		slice string
+		from  int
+		to    int
+	}{
+		{":", 0, 0},
+		{"0:1", 0, 1},
+		{":1", 0, 1},
+		{"0:", 0, 0},
+		{"0:-1", 0, -1},
+		{"-2:-1", -2, -1},
+		{"-2:1", -2, 1},
+	}
+	for _, tc := range tcs {
+		t.Run(tc.slice, func(t *testing.T) {
+			from, to, err := parseArgs([]string{tc.slice})
+			if err != nil {
+				t.Errorf("got error %v", err)
+			}
+			if from != tc.from {
+				t.Errorf("from is %d, expected %d", from, tc.from)
+			}
+			if to != tc.to {
+				t.Errorf("to is %d, expected %d", to, tc.to)
+			}
+		})
+	}
+}
+
+func TestParseArgsError(t *testing.T) {
+	tcs := []struct {
+		slice string
+	}{
+		{"abc"},
+		{"abc:"},
+		{"abc:123"},
+		{":abc"},
+		{"1:2:"},
+		{":2:"},
+		{"1:2:"},
+		{":2:3"},
+		{"1:2:3"},
+	}
+	for _, tc := range tcs {
+		t.Run(tc.slice, func(t *testing.T) {
+			if _, _, err := parseArgs([]string{tc.slice}); err == nil {
+				t.Error("expected error, but was nil")
+			}
+		})
+	}
+}
+
+func TestSliceLine(t *testing.T) {
+	tcs := []struct {
+		line  string
+		slice string
+		from  int
+		to    int
+	}{
+		{"", "", 0, 0},
+		{"a", "a", -1, 0},
+		{"ab", "b", -1, 0},
+		{"ab", "a", 0, 1},
+		{"ab", "ab", 0, 2},
+	}
+	for _, tc := range tcs {
+		t.Run(tc.line, func(t *testing.T) {
+			if s := sliceLine(tc.line, tc.from, tc.to); s != tc.slice {
+				t.Errorf("expected '%s', but was '%s'", tc.slice, s)
+			}
+		})
+	}
+}
